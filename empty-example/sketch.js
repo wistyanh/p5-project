4f43f7b673ff97e190a6af0b578352f3da135aca
Anh Pham <wistyanh12@gmail.com>
2021-02-13 12:47:35 -0800

    added game-over screen & moving enemies

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,24 +1,24 @@
 
 //mic test that actually works to move bal up and down
-let mic;
+// let mic;
 
- function setup(){
-  let cnv = createCanvas(100, 100);
-  cnv.mousePressed(userStartAudio);
-  textAlign(CENTER);
-  mic = new p5.AudioIn();
-  mic.start();
-}
+//  function setup(){
+//   let cnv = createCanvas(100, 100);
+//   cnv.mousePressed(userStartAudio);
+//   textAlign(CENTER);
+//   mic = new p5.AudioIn();
+//   mic.start();
+// }
 
-function draw(){
-  background(0);
-  fill(255);
-  text('tap to start', width/2, 20);
+// function draw(){
+//   background(0);
+//   fill(255);
+//   text('tap to start', width/2, 20);
 
-  micLevel = mic.getLevel();
-  let y = height - micLevel * height;
-  ellipse(width/2, y, 10, 10);
-}
+//   micLevel = mic.getLevel();
+//   let y = height - micLevel * height;
+//   ellipse(width/2, y, 10, 10);
+// }
 
 
 
@@ -32,36 +32,87 @@ function draw(){
 2. add the obstacle course -- prog incoming obstacles either must jump or 
 */
 
-/*
+
 let mic;
 let button;
-let gameStatus = false;
+let gameStatus;
+let newHeight = 200;
+let newWidth = 1000;
 
  function setup(){
    mic = new p5.AudioIn();
-   let cnv = createCanvas(750, 750);
+   createCanvas(1000, 750);
    background("#cce6ff");
-   textFont('Helvetica', 40)
-   text('skreem', 300, 100);
-   button = createButton('click to start releasing ✨stress✨');
-   button.position(277, 375);
+   textFont('Helvetica', 50)
+   text('✨skreem✨', 365, 200);
+   
+   textSize(20);
+   text('sorry this is ugly asf im artistically challenged', 575, 725);
+   
+   button = createButton('click here to begin!');
+   button.position(440, 375);
    button.mousePressed(startGame);
    
 }
 
 function draw(){
+  let pHeight = second();
+  let moving = false;
+  let player;
+  let playerOnGround = true;
+  
   
   if (gameStatus) {
-    background("#ccccff")
-    fill(255);
-    rect(0, 700, 750, 50);
-    micLevel = mic.getLevel();
-  
-    let y = max(50, (height - micLevel * 1000));
-    console.log(y);
-    ellipse(width/2, y, 10, 10);
+    background("#ccccff");
+    rect(0, 700, 1000, 50);
+    if (!moving) {
+      micLevel = mic.getLevel() * 1000;
+    } else {
+      
+    }
+    
+    
+    
+//     if (!moving) {
+//       pHeight = height - micLevel * height;
+//       moving = true;
+//     }
+    
+//     if (moving) {
+//       player = ellipse(width/4, min(pHeight, 690), 20, 20);
+//       playerOnGround = false;
+//       if ()
+//     }
+    
+    //pHeight = height - micLevel * height;
+    //let player = ellipse(width/4, min(pHeight, 690), 20, 20);
+    
+    //rect(x_start_from_left, y_start_from_top, w, h);
+    
+    
+    newWidth = newWidth - 5;
+    let enemy = rect(newWidth, 700-newHeight, 50, newHeight);
+    
+    if (newWidth <= -50) {
+      newWidth = 1000;
+      newHeight = random(150, 500);
+    }
   }
+  
+  if (gameStatus == false) {
+    background("#ccccff");
+    textFont('Helvetica', 100)
+    text('GAME OVER', 200, 200);
+  
+    button = createButton('click here to restart!');
+    button.position(440, 375);
+    button.mousePressed(startGame);
+    
+  }
+  
 }
+
+
 
 function startGame() {
   button.hide();
@@ -69,4 +120,5 @@ function startGame() {
   mic.start();
   gameStatus = true;
 }
-*/
+
+
