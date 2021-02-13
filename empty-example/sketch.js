8d3bb5f6edbd4e8d694eb3d6bbfabe8d39754598
Anh Pham <wistyanh12@gmail.com>
2021-02-13 13:07:10 -0800

    moving enemies, has game over screen, but need to fix player

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,38 +1,10 @@
-
-//mic test that actually works to move bal up and down
-// let mic;
-
-//  function setup(){
-//   let cnv = createCanvas(100, 100);
-//   cnv.mousePressed(userStartAudio);
-//   textAlign(CENTER);
-//   mic = new p5.AudioIn();
-//   mic.start();
-// }
-
-// function draw(){
-//   background(0);
-//   fill(255);
-//   text('tap to start', width/2, 20);
-
-//   micLevel = mic.getLevel();
-//   let y = height - micLevel * height;
-//   ellipse(width/2, y, 10, 10);
-// }
-
-
-
 //only works on p5 editor and idk why
 /* problems:
 1. localhost simply isn't working
-2. is there a way for me to like reduce the noise to make the ball less shaky and more stable?
+2. slow down player
+3. add collision detector
+4. restart button won't go away
 */
-/* 
-1. look for a way to stabilize ball
-2. add the obstacle course -- prog incoming obstacles either must jump or 
-*/
-
-
 let mic;
 let button;
 let gameStatus;
@@ -57,35 +29,16 @@ let newWidth = 1000;
 
 function draw(){
   let pHeight = second();
-  let moving = false;
-  let player;
-  let playerOnGround = true;
   
   
   if (gameStatus) {
     background("#ccccff");
     rect(0, 700, 1000, 50);
-    if (!moving) {
-      micLevel = mic.getLevel() * 1000;
-    } else {
-      
-    }
+    micLevel = mic.getLevel() * 5;
     
     
-    
-//     if (!moving) {
-//       pHeight = height - micLevel * height;
-//       moving = true;
-//     }
-    
-//     if (moving) {
-//       player = ellipse(width/4, min(pHeight, 690), 20, 20);
-//       playerOnGround = false;
-//       if ()
-//     }
-    
-    //pHeight = height - micLevel * height;
-    //let player = ellipse(width/4, min(pHeight, 690), 20, 20);
+    pHeight = height - micLevel * height;
+    let player = ellipse(width/4, min(pHeight, 690), 20, 20);
     
     //rect(x_start_from_left, y_start_from_top, w, h);
     
