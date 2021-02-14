d167fd6498df34d43b19504ffc76f5ef39b73b6f
Anh Pham <wistyanh12@gmail.com>
2021-02-14 11:25:35 -0800

    finisehd p5 but mic is very bipolar

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,22 +1,17 @@
-//only works on p5 editor and idk why
-/* problems:
-3. slow down player
-4. add collision detector
-*/
 let mic;
 let button1;
 let button2;
 let gameStatus;
-let enemyHeight = 200;
-let enemyWidth = 1000;
-let restart = false;
+let enemyHeight;
+let enemyWidth;
+let restart;
 
 let pX;
-let pY = 690;
-let xVel = 0;
-let yVel = 0;
-let xAcc = 0;
-let yAcc = 1;
+let pY;
+let xVel;
+let yVel;
+let xAcc;
+let yAcc;
 
  function setup(){
    mic = new p5.AudioIn();
@@ -42,9 +37,6 @@ let yAcc = 1;
 function draw(){
   let sec = second();
   let enemyYStart;
-  pX = width/4;
-  
-  
   
   if (gameStatus) {
     background("#ccccff");
@@ -55,7 +47,7 @@ function draw(){
     
     let player = ellipse(pX, pY, 20, 20);
     
-    if (micLevel > 2) {
+    if (micLevel > 7) {
       
       yAcc = -.1;
     } else {
@@ -68,19 +60,10 @@ function draw(){
     
     if (pY > 690) {
       yVel = yVel * -1;
-    } else if (pY < 0) {
-      yVel = (yVel * -1)  / 4;
+    } else if (pY <= 5) {
+      yVel = 0.15 ;
     }
   
-    
-    // micLevel = mic.getLevel() * 10;
-    // console.log(micLevel);
-    // let pHeight = height - micLevel * height;
-    // let player = ellipse(width/4, min(pHeight, 690), 20, 20);
-    
-    
-    
-    
     enemyWidth = enemyWidth - 5;
     enemyYStart = 700-enemyHeight;
     let enemy = rect(enemyWidth, enemyYStart, 50, enemyHeight);
@@ -89,20 +72,22 @@ function draw(){
       enemyWidth = 1000;
       enemyHeight = random(150, 500);
     }
+
+    if (pX+20 >= enemyWidth && pX+20 <= enemyWidth+50 && pY >= enemyYStart) {
+      gameStatus = false;
+    }
+    
   }
   
   if (gameStatus == false) {
     background("#ccccff");
     textFont('Helvetica', 100)
     text('GAME OVER', 200, 200);
-    restart =  true;
-  
-    button2.show();
-    
-  }
-  
-}
 
+    restart =  true;
+    button2.show();
+  }
+}
 
 
 function startGame() {
@@ -112,6 +97,135 @@ function startGame() {
     restart = false;
   }
   background("#ccccff");
+  enemyHeight = 200;
+  enemyWidth = 1000;
+  restart = false;
+
+  pX = width/4;;
+  pY = 690;
+  xVel = 0;
+  yVel = 0;
+  xAcc = 0;
+  yAcc = 0.1;
   mic.start();
   gameStatus = true;
 }
+
+
+
+
+
+
+
+// let mic;
+// let button1;
+// let button2;
+// let gameStatus;
+// let enemyHeight = 200;
+// let enemyWidth = 1000;
+// let restart = false;
+
+// let pX;
+// let pY = 690;
+// let xVel = 0;
+// let yVel = 0;
+// let xAcc = 0;
+// let yAcc = 0.1;
+
+//  function setup(){
+//    mic = new p5.AudioIn();
+//    createCanvas(1000, 750);
+//    background("#cce6ff");
+//    textFont('Helvetica', 50)
+//    text('✨skreem✨', 365, 200);
+   
+//    textSize(20);
+//    text('sorry this is ugly asf im artistically challenged', 575, 725);
+   
+//    button1 = createButton('click here to begin!');
+//    button1.position(440, 375);
+//    button1.mousePressed(startGame);
+   
+//    button2 = createButton('click here to restart!');
+//    button2.position(440, 375);
+//    button2.hide();
+//    button2.mousePressed(startGame);
+   
+// }
+
+// function draw(){
+//   let sec = second();
+//   let enemyYStart;
+//   pX = width/4;
+  
+  
+  
+//   if (gameStatus) {
+//     background("#ccccff");
+//     rect(0, 700, 1000, 50);
+
+//     micLevel = mic.getLevel() * 100;
+//     console.log("micLevel is " + micLevel);
+    
+//     let player = ellipse(pX, pY, 20, 20);
+    
+//     if (micLevel > 2) {
+      
+//       yAcc = -.1;
+//     } else {
+//       yAcc = .2;
+//     }
+    
+//     pY += yVel;
+//     yVel += yAcc;
+//     console.log("pY is " + pY + " yVel is " + yVel);
+    
+//     if (pY > 690) {
+//       yVel = yVel * -1;
+//     } else if (pY <= 5) {
+//       yVel = 0.15 ;
+//     }
+  
+    
+//     // micLevel = mic.getLevel() * 10;
+//     // console.log(micLevel);
+//     // let pHeight = height - micLevel * height;
+//     // let player = ellipse(width/4, min(pHeight, 690), 20, 20);
+    
+    
+    
+    
+//     enemyWidth = enemyWidth - 5;
+//     enemyYStart = 700-enemyHeight;
+//     let enemy = rect(enemyWidth, enemyYStart, 50, enemyHeight);
+    
+//     if (enemyWidth <= -50) {
+//       enemyWidth = 1000;
+//       enemyHeight = random(150, 500);
+//     }
+//   }
+  
+//   if (gameStatus == false) {
+//     background("#ccccff");
+//     textFont('Helvetica', 100)
+//     text('GAME OVER', 200, 200);
+//     restart =  true;
+  
+//     button2.show();
+    
+//   }
+  
+// }
+
+
+
+// function startGame() {
+//   button1.hide();
+//   if (restart) {
+//     button2.hide();
+//     restart = false;
+//   }
+//   background("#ccccff");
+//   mic.start();
+//   gameStatus = true;
+// }
